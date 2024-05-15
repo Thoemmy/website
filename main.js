f0436c08f2df5fd87c4144a55675230027b0b631
Thoemmy <89771415+Thoemmy@users.noreply.github.com>
2024-05-15 10:32:17 +0200

    Update main.js
    Update jsonData to show new website information

--- main.js
+++ main.js
@@ -16,79 +16,103 @@ document.addEventListener("DOMContentLoaded", function () {
   }, 1500);
 
   var jsonData = [
-    {
-      id: "name",
-      class: "",
-      text: "NAME: Thomas Kopitsch",
-      element: "h3",
-    },
-    {
-      id: "alias",
-      class: "",
-      text: "KNOWN ALIAS: thoemmy",
-      element: "h3",
-    },
-    {
-      id: "occupation",
-      class: "",
-      text: "OCCUPATION: FULL STACK DEVELOPER",
-      element: "h3",
-    },
-    {
-      id: "",
-      class: "",
-      text: "",
-      element: "br",
-    },
-    {
-      id: "fullstack-end-span",
-      class: "fa fa-wrench",
-      text: "",
-      element: "i",
-    },
-    {
-      id: "fullstack-end",
-      class: "inline",
-      text: "FULL STACK DEVELOPMENT CORE:",
-      element: "h3",
-    },
-    {
-      id: "js-frameworks",
-      class: "sub-item",
-      text: "JAVASCRIPT FRAMEWORKS: ANGULAR | NESTJS",
-      element: "h4",
-    },
-    {
-      id: "ui-frameworks",
-      class: "sub-item",
-      text: "UI FRAMEWORKS: PRIMENG",
-      element: "h4",
-    },
-    {
-      id: "js-libs",
-      class: "sub-item",
-      text: "JAVASCRIPT LIBRARIES: JQUERY",
-      element: "h4",
-    },
-    {
-      id: "css-frameworks",
-      class: "sub-item",
-      text: "CSS FRAMEWORKS: PRIMEFLEX",
-      element: "h4",
-    },
-    {
-      id: "css-pre",
-      class: "sub-item",
-      text: "CSS PREPROCESSORS: SASS",
-      element: "h4",
-    },
-    {
-      id: "",
-      class: "",
-      text: "",
-      element: "br",
-    },
-  ];
+  {
+    id: 'name',
+    class: '',
+    text: 'NAME: Thomas Kopitsch',
+    element: 'h3',
+  },
+  {
+    id: 'alias',
+    class: '',
+    text: 'KNOWN ALIAS: thoemmy',
+    element: 'h3',
+  },
+  {
+    id: 'occupation',
+    class: '',
+    text: 'OCCUPATION: FULL STACK DEVELOPER',
+    element: 'h3',
+  },
+  {
+    id: '',
+    class: '',
+    text: '',
+    element: 'br',
+  },
+  {
+    id: 'fullstack-end-span',
+    class: 'fa fa-wrench',
+    text: '',
+    element: 'i',
+  },
+  {
+    id: 'fullstack-end',
+    class: 'inline',
+    text: 'FULL STACK DEVELOPMENT CORE:',
+    element: 'h3',
+  },
+  {
+    id: 'js-frameworks',
+    class: 'sub-item',
+    text: 'JAVASCRIPT FRAMEWORKS: ANGULAR | NESTJS',
+    element: 'h4',
+  },
+  {
+    id: 'ui-frameworks',
+    class: 'sub-item',
+    text: 'UI FRAMEWORKS: PRIMENG',
+    element: 'h4',
+  },
+  {
+    id: 'js-libs',
+    class: 'sub-item',
+    text: 'JAVASCRIPT LIBRARIES: JQUERY',
+    element: 'h4',
+  },
+  {
+    id: 'css-frameworks',
+    class: 'sub-item',
+    text: 'CSS FRAMEWORKS: PRIMEFLEX',
+    element: 'h4',
+  },
+  {
+    id: 'css-pre',
+    class: 'sub-item',
+    text: 'CSS PREPROCESSORS: SASS',
+    element: 'h4',
+  },
+  {
+    id: '',
+    class: '',
+    text: '',
+    element: 'br',
+  },
+  {
+    id: 'discord-span',
+    class: 'fa fa-gear',
+    text: '',
+    element: 'i',
+  },
+  {
+    id: 'discord-end',
+    class: 'inline',
+    text: 'DISCORD BOT DEVELOPMENT CORE:',
+    element: 'h3',
+  },
+  {
+    id: 'discord-frameworks',
+    class: 'sub-item',
+    text: 'PYTHON FRAMEWORKS: Discord.py | Pycord',
+    element: 'h4',
+  },
+  {
+    id: 'discord-usecases',
+    class: 'sub-item',
+    text: 'BOT EXAMPLE FUNCTIONS: Server Management, CryptoGame Stats, Gamestats, LiveTicker, Scholar Management System',
+    element: 'h4',
+  },
+];
 
   function initElements(data, index) {
     var elementData = data[index];
